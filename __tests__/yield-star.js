f47feaa7db2673718bede4b2257542c9f1de95db
ttmarek <ttmarek@outlook.com>
2017-12-18 10:04:01 -0500

    Add yield-star example

---
+++ __tests__/yield-star.js
@@ -0,0 +1,35 @@
+import {
+  put,
+  call,
+  takeEvery,
+} from 'redux-saga/effects';
+import getConfiguredStore from '../utils/get-configured-store';
+
+
+test('...', () => {
+
+  const fruits = [];
+
+  function* sagaA(arrayOfFruits) {
+    for (let i = 0; i < arrayOfFruits.length; i++) {
+      yield fruits.push(arrayOfFruits[i]);
+    }
+  }
+
+  function* sagaB() {
+    yield fruits.push('banana');
+    // yield call(sagaA, ['apple', 'kiwi', 'pineapple']);
+    // yield* sagaA(['apple', 'kiwi', 'pineapple']);
+    yield fruits.push('mango');
+  }
+
+  const { reduxStore } = getConfiguredStore({}, sagaB);
+
+  expect(fruits).toEqual([
+    'banana',
+    'apple',
+    'kiwi',
+    'pineapple',
+    'mango',
+  ]);
+});
